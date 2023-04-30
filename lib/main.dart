04563f2f3c37ee44ff11c6e6973951442379b3b7
Lenz Paul <bringbackthedog@gmail.com>
2023-04-29 22:04:59 -0400

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MainApp());
@@ -22,23 +23,89 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class HomeSreen extends StatelessWidget {
+class HomeSreen extends StatefulWidget {
   HomeSreen({
     super.key,
   });
 
   @override
+  State<HomeSreen> createState() => _HomeSreenState();
+}
+
+class _HomeSreenState extends State<HomeSreen> {
+  final filePaths = Utils.listFiles();
+
+  int _currentlySelectedIdx = 0;
+
+  int get currentlySelectedIdx => _currentlySelectedIdx;
+
+  set currentlySelectedIdx(int idx) {
+    if (idx < 0) {
+      return;
+    }
+
+    if (idx >= filePaths.length) {
+      return;
+    }
+
+    _currentlySelectedIdx = idx;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final filePaths = Utils.listFiles();
+    final _focusNode = FocusNode();
+
+    /// Change the focuss when the user presses up or down arrow keys
+    RawKeyboardListener(
+      focusNode: _focusNode,
+      onKey: (event) {
+        if (event.isKeyPressed(LogicalKeyboardKey.arrowUp)) {
+          print('Up');
+        } else if (event.isKeyPressed(LogicalKeyboardKey.arrowDown)) {
+          print('Down');
+        }
+      },
+      child: Container(),
+    );
 
     return Scaffold(
-      body: ListView.builder(
-        itemCount: filePaths.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(filePaths[index]),
-          );
-        },
+      body: Column(
+        children: [
+          Text(Utils.currentPath),
+          Flexible(
+            child: Focus(
+              onKey: (node, event) {
+                if (event.isKeyPressed(LogicalKeyboardKey.arrowUp)) {
+                  print('Up');
+                  // TODO Change the focus to the previous item
+                  currentlySelectedIdx = currentlySelectedIdx - 1;
+                  setState(() {});
+                } else if (event.isKeyPressed(LogicalKeyboardKey.arrowDown)) {
+                  print('Down');
+                  currentlySelectedIdx = currentlySelectedIdx + 1;
+                  setState(() {});
+                }
+
+                return KeyEventResult.handled;
+              },
+              autofocus: true,
+              focusNode: _focusNode,
+              child: ListView.builder(
+                itemCount: filePaths.length,
+                itemBuilder: (context, index) {
+                  if (index == currentlySelectedIdx) {
+                    return Container(
+                      color: Colors.blue,
+                      child: Text(filePaths[index].name),
+                    );
+                  }
+
+                  return Text(filePaths[index].name);
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -72,6 +139,14 @@ class NavBar extends StatelessWidget {
 }
 
 class Utils {
+  static String _currentPath = homeDir;
+
+  static get currentPath => _currentPath;
+
+  static void changePath(String newPath) {
+    _currentPath = newPath;
+  }
+
   /// Get the user's home directory path
   static String get homeDir {
     String? homeDir = Platform.environment['HOME'];
@@ -84,35 +159,55 @@ class Utils {
   }
 
   /// List all files in home directory
-  static List<String> listFiles({String? path, bool showHidden = false}) {
+  static List<FileSystemEntity> listFiles(
+      {String? path, bool showHidden = false}) {
     path ??= homeDir;
 
-    var filePaths = <String>[];
+    // var filePaths = <String>[];
+    // var files = <FileSystemEntity>[];
 
     final dir = Directory(path);
-    final files = dir.listSync();
-    for (var file in files) {
+    final files = dir.listSync().where((file) {
       bool isFile = FileSystemEntity.isFileSync(file.path);
-      // bool isDir = FileSystemEntity.isDirectorySync(file.path);
-
-      // Remove the parent directory from the path
+      bool isDir = FileSystemEntity.isDirectorySync(file.path);
       var fileName = file.path.split('/').last;
 
       // Remove directories
       if (!isFile) {
-        continue;
+        return false;
       }
 
       // Don't add hidden files and directories
       if (fileName.startsWith('.') && !showHidden) {
-        continue;
+        return false;
       }
 
-      filePaths.add(fileName);
-      // filePaths.add(file.path);
-    }
+      return true;
+    }).toList();
 
-    return filePaths;
+    // for (var file in files) {
+    //   bool isFile = FileSystemEntity.isFileSync(file.path);
+    //   // bool isDir = FileSystemEntity.isDirectorySync(file.path);
+
+    //   // Remove the parent directory from the path
+    //   var fileName = file.path.split('/').last;
+
+    //   // Remove directories
+    //   if (!isFile) {
+    //     continue;
+    //   }
+
+    //   // Don't add hidden files and directories
+    //   if (fileName.startsWith('.') && !showHidden) {
+    //     continue;
+    //   }
+
+    //   // filePaths.add(fileName);
+    //   files.add(file);
+    // }
+
+    // return filePaths;
+    return files;
   }
 
   static bool isMacOS() {
@@ -134,4 +229,9 @@ class Utils {
       print('$key: $value');
     });
   }
+}
+
+extension on FileSystemEntity {
+  /// Name without the path
+  String get name => path.split('/').last;
 }
