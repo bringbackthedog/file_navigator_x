f3f7c8ea7cd19a8028670c36f4c9198261247c1e
Lenz Paul <bringbackthedog@gmail.com>
2023-04-29 22:53:56 -0400

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -6,107 +6,174 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
-  runApp(MainApp());
-}
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class MainApp extends StatelessWidget {
-  MainApp({super.key});
+void main() => runApp(const KeyExampleApp());
 
-  // final bool isMacOS = defaultTargetPlatform == TargetPlatform.macOS;
+class KeyExampleApp extends StatelessWidget {
+  const KeyExampleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeSreen(),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Key Handling Example')),
+        body: const MyKeyExample(),
+      ),
     );
   }
 }
 
-class HomeSreen extends StatefulWidget {
-  HomeSreen({
-    super.key,
-  });
+class MyKeyExample extends StatefulWidget {
+  const MyKeyExample({super.key});
 
   @override
-  State<HomeSreen> createState() => _HomeSreenState();
+  State<MyKeyExample> createState() => _MyKeyExampleState();
 }
 
-class _HomeSreenState extends State<HomeSreen> {
-  final filePaths = Utils.listFiles();
+class _MyKeyExampleState extends State<MyKeyExample> {
+  // The node used to request the keyboard focus.
+  final FocusNode _focusNode = FocusNode();
 
-  int _currentlySelectedIdx = 0;
+  var files = Utils.listDirContent();
 
-  int get currentlySelectedIdx => _currentlySelectedIdx;
+  int _currentSelection = 0;
 
-  set currentlySelectedIdx(int idx) {
-    if (idx < 0) {
+  int get currentSelection => _currentSelection;
+
+  static int count = 0;
+  set currentSelection(int index) {
+    if (index < 0 || index >= files.length) {
       return;
     }
+    // setState(() {
+    _currentSelection = index;
+    // });
+  }
 
-    if (idx >= filePaths.length) {
-      return;
+  // Focus nodes need to be disposed.
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  // Handles the key events from the Focus widget and updates the
+  // _message.
+  KeyEventResult _handleKeyEvent(FocusNode node, RawKeyEvent event) {
+    if (event is! RawKeyDownEvent) {
+      return KeyEventResult.skipRemainingHandlers;
     }
 
-    _currentlySelectedIdx = idx;
+    switch (event.logicalKey) {
+      case LogicalKeyboardKey.arrowUp:
+        currentSelection--;
+        break;
+
+      case LogicalKeyboardKey.arrowDown:
+        currentSelection++;
+        break;
+
+      case LogicalKeyboardKey.enter:
+        break;
+
+      case LogicalKeyboardKey.arrowRight:
+        // Open file or directory
+        if (FileSystemEntity.isDirectorySync(files[currentSelection].path)) {
+          Utils.changePath(files[currentSelection].path);
+          files = Utils.listDirContent(path: Utils.currentPath);
+          currentSelection = 0;
+        }
+        setState(() {});
+
+        break;
+
+      case LogicalKeyboardKey.arrowLeft:
+        // Go back
+        if (Utils.currentPath != Utils.homeDir) {
+          Utils.changePath(Utils.currentPath
+              .split('/')
+              .sublist(0, Utils.currentPath.split('/').length - 1)
+              .join('/'));
+          files = Utils.listDirContent(path: Utils.currentPath);
+          currentSelection = 0;
+        }
+        setState(() {});
+
+        break;
+
+      default:
+    }
+    // setState(
+    //   () {
+    //     if (event.logicalKey == LogicalKeyboardKey.arrowUp) {
+    //       currentSelection--;
+    //     } else if (event.logicalKey == LogicalKeyboardKey.arrowDown) {
+    //       currentSelection++;
+    //     }
+    //   },
+    // );
+
+    setState(() {});
+
+    // Returning true tells the framework that we handled the event.
+    // Otherwise, the key event cascades to the widgets in the focused
+    // widget's subtree.
+    return KeyEventResult.handled;
   }
 
   @override
   Widget build(BuildContext context) {
-    final _focusNode = FocusNode();
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return Column(
+      children: [
+        Text('Current path: ${Utils.currentPath}'),
+        Expanded(
+          child: Container(
+            color: Colors.white,
+            alignment: Alignment.center,
+            child: DefaultTextStyle(
+              style: textTheme.bodyLarge!.copyWith(
+                fontFamily: 'monospace',
+                fontSize: 16.0,
+              ),
+              child: Focus(
+                focusNode: _focusNode,
+                onKey: _handleKeyEvent,
+                child: AnimatedBuilder(
+                  animation: _focusNode,
+                  builder: (BuildContext context, Widget? child) {
+                    if (!_focusNode.hasFocus) {
+                      return GestureDetector(
+                        onTap: () {
+                          FocusScope.of(context).requestFocus(_focusNode);
+                        },
+                        child: const Text('Click to focus'),
+                      );
+                    }
+                    return ListView.builder(
+                      itemCount: files.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          color: index == _currentSelection
+                              // ? Colors.blue.withOpacity(0.3)
+                              ? Colors.blue
+                              : Colors.transparent,
+                          child: Text(files[index].name),
+                        );
 
-    /// Change the focuss when the user presses up or down arrow keys
-    RawKeyboardListener(
-      focusNode: _focusNode,
-      onKey: (event) {
-        if (event.isKeyPressed(LogicalKeyboardKey.arrowUp)) {
-          print('Up');
-        } else if (event.isKeyPressed(LogicalKeyboardKey.arrowDown)) {
-          print('Down');
-        }
-      },
-      child: Container(),
-    );
-
-    return Scaffold(
-      body: Column(
-        children: [
-          Text(Utils.currentPath),
-          Flexible(
-            child: Focus(
-              onKey: (node, event) {
-                if (event.isKeyPressed(LogicalKeyboardKey.arrowUp)) {
-                  print('Up');
-                  // TODO Change the focus to the previous item
-                  currentlySelectedIdx = currentlySelectedIdx - 1;
-                  setState(() {});
-                } else if (event.isKeyPressed(LogicalKeyboardKey.arrowDown)) {
-                  print('Down');
-                  currentlySelectedIdx = currentlySelectedIdx + 1;
-                  setState(() {});
-                }
-
-                return KeyEventResult.handled;
-              },
-              autofocus: true,
-              focusNode: _focusNode,
-              child: ListView.builder(
-                itemCount: filePaths.length,
-                itemBuilder: (context, index) {
-                  if (index == currentlySelectedIdx) {
-                    return Container(
-                      color: Colors.blue,
-                      child: Text(filePaths[index].name),
+                        // return Text(files[index].path);
+                      },
                     );
-                  }
-
-                  return Text(filePaths[index].name);
-                },
+                  },
+                ),
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
@@ -159,8 +226,12 @@ class Utils {
   }
 
   /// List all files in home directory
-  static List<FileSystemEntity> listFiles(
-      {String? path, bool showHidden = false}) {
+  static List<FileSystemEntity> listDirContent({
+    String? path,
+    bool showHidden = false,
+    bool showDirectories = true,
+    bool showFiles = true,
+  }) {
     path ??= homeDir;
 
     // var filePaths = <String>[];
@@ -170,15 +241,20 @@ class Utils {
     final files = dir.listSync().where((file) {
       bool isFile = FileSystemEntity.isFileSync(file.path);
       bool isDir = FileSystemEntity.isDirectorySync(file.path);
-      var fileName = file.path.split('/').last;
+
+      var entityName = file.path.split('/').last;
 
       // Remove directories
-      if (!isFile) {
+      if (!isFile && !isDir) {
+        return false;
+      } else if (isDir && !showDirectories) {
+        return false;
+      } else if (isFile && !showFiles) {
         return false;
       }
 
       // Don't add hidden files and directories
-      if (fileName.startsWith('.') && !showHidden) {
+      if (entityName.startsWith('.') && !showHidden) {
         return false;
       }
 
